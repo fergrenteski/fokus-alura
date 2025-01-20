067d60d8ddfad4a5d774d4f5e96f61224cf90ce4
Luiz Fernando <luizgrenfer@gmail.com>
2025-01-20 20:35:53 -0300

    :bulb: docs:
     - Adicionado comentários no código.

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
 const html = document.querySelector('html');
 
+// Botões
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const btns = document.querySelectorAll('.app__card-button');
 
+// Banner
 const banner = document.querySelector('.app__image');
 
+// Título
 const title = document.querySelector('.app__title');
 
+// Temporizadores
 const focoTemp = 1500;
 const longoTemp = 900;
 const curtoTemp = 300;
 
+// Evento do Botão de Foco
 focoBtn.addEventListener('click', () => {
     switchContext('foco');
     focoBtn.classList.add('active');
 });
 
+// Evento do Botão de Curto
 curtoBtn.addEventListener('click', () => {
     switchContext('descanso-curto');
     curtoBtn.classList.add('active');
 });
 
+// Evento do Botão de Longo
 longoBtn.addEventListener('click', () => {
     switchContext('descanso-longo');
     longoBtn.classList.add('active');
 });
 
+/**
+ * Define o contexto através do nome.
+ * @param {String} contexto Nome.
+ */
 function switchContext(contexto) {
     btns.forEach(function(btn) {
         btn.classList.remove('active');
     })
-    
+
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
 
