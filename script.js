dbf3b31c76ccd88083568675be96ef5dd31a290d
Luiz Fernando <luizgrenfer@gmail.com>
2025-01-20 20:25:56 -0300

    :sparkles: feat:
     - Adicionado alteração de titulo com switch.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const longoBtn = document.querySelector('.app__card-button--longo');
 
 const banner = document.querySelector('.app__image');
 
+const title = document.querySelector('.app__title');
+
 const focoTemp = 1500;
 const longoTemp = 900;
 const curtoTemp = 300;
@@ -25,4 +27,21 @@ longoBtn.addEventListener('click', () => {
 function switchContext(contexto) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
+
+    switch (contexto) {
+        case 'foco':
+            title.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case 'descanso-curto':
+            title.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case 'descanso-longo':
+            title.innerHTML = `Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa!</strong>`
+            break;
+        default:
+            break;
+    }
 }
